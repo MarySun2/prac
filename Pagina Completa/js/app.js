50bf7b7f357035a53230be40baedade9dcc984a1
Mary <marisol.pereira.goncalves@gmail.com>
2024-11-15 16:46:39 +0100

    Perzonalizar Web + css + js

---
+++ Pagina Completa/js/app.js
@@ -0,0 +1,7 @@
+// Importar Popper.js
+import "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js";
+
+// Importar Bootstrap JS
+import "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js";
+
+console.log("Popper.js y Bootstrap cargados correctamente.");
